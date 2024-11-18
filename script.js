dd6ec1cd6a44226d16bc811849fad17e29521c04
adodoCoder <alavropulido@whccd.edu>
2024-11-17 22:04:26 -0800

    added last section of code

--- script.js
+++ script.js
@@ -15,22 +15,21 @@
 const ballon = document.getElementById("balloon");
 
 
-let fontSize = 30;
+let fontSize = 30; // starting size for the balloon */
 balloon.style.fontSize = fontSize + 'px';
 function handleKeydown(event) {
     if (event.key == 'ArrowUp') {
-        if (fontSize > 30){
-            balloon.textContent = "💥";
+        if (fontSize > 30){ // The size the balloon will pop at */
+            balloon.textContent = "💥"; // when popped the down arrow will not throw the event */
             removeEventListener('keydown', handleKeydown);
 
         }else{
-            fontSize *= 1.1;
+            fontSize *= 1.1; // the size the balloon will grow when the arrow key up is pressed */
             balloon.style.fontSize = fontSize + 'px';
             event.preventDefault();
         }
     }
-}
-else if (event.key == 'ArrowDown') {
+}else if (event.key == 'ArrowDown') { // if arrow key down is pressed it will decrease by this size */
     fontSize *= 0.9;
     balloon.style.fontSize = fontSize + 'px';
     event.preventDefault();
@@ -46,3 +45,36 @@ window.addEventListener('keydown', handleKeydown);
 // function as expected. There are many ways to accomplish this task, but you will need
 // to at minimum add listeners to each link and toggle the display of the tab contents.
 // Hint: display: none; hides an element, and display: block; will bring it
+
+
+
+// selects the tabs */
+const tab1 = document.querySelector('#tab1');
+const tab2 = document.querySelector('#tab2');
+const tab3 = document.querySelector('#tab3');
+// selects the link tab*/
+const tab1link = document.querySelector('#tab1Link');
+const tab2link = document.querySelector('#tab2Link');
+const tab3link = document.querySelector('#tab3Link');
+
+function showtab(tabtoshow){
+  tab1.style.display = "none";// hides the tabs not selected  */
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+  tabtoshow.style.display = "block";// this will be shown when selected */
+}
+// starts with tab 1 default */
+showtab(tab1);
+// event listeners to tabs when tab is selected */
+
+tab1link.addEventListener("click", function(event){
+  event.preventDefault();
+  showtab(tab1);
+});
+tab2link.addEventListener("click", function(event){
+  event.preventDefault();
+  showtab(tab2);
+});
+tab3link.addEventListener("click",function(event){
+  event.preventDefault();
+  showtab(tab3)
